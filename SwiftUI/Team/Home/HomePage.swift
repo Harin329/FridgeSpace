26b65ba921b3c72a9939b0941dcbc7305356bdf7
HarinHaoWu <master.harin@gmail.com>
2021-01-15 22:40:11 -0800

    Photo Taking

--- SwiftUI/Team/Home/HomePage.swift
+++ SwiftUI/Team/Home/HomePage.swift
@@ -11,47 +11,80 @@ struct HomePage : View {
     
     @Binding var x : CGFloat
     @Binding var showing: Int
+    
+    @State var showCaptureImageView: Bool = false
+    @State var image: Image?
     var body: some View{
         // Home View With CUstom Nav bar...
-        VStack{
-            HStack{
-                Button(action: {
-                    // opening menu,...
-                    withAnimation{
-                        x = 0
+        if (showCaptureImageView) {
+            CaptureImageView(isShown: $showCaptureImageView, image: $image)
+        } else {
+            VStack{
+                HStack{
+                    Button(action: {
+                        // opening menu,...
+                        withAnimation{
+                            x = 0
+                        }
+                    }) {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
+                            .padding(.leading, 10)
                     }
-                }) {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.system(size: 24))
+                    Spacer(minLength: 0)
+                    Text("Food Buds")
+                        .fontWeight(.bold)
                         .foregroundColor(.black)
-                        .padding(.leading, 10)
+                    Spacer(minLength: 0)
+                    Button(action: {
+                        self.showCaptureImageView.toggle()
+                    }) {
+                        Image(systemName: "camera")
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
+                            .padding(.trailing, 10)
+                    }
                 }
-                Spacer(minLength: 0)
-                Text("Food Buds")
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                Spacer(minLength: 0)
-                Button(action: {
-                    // Todo
-                }) {
-                    Image(systemName: "camera")
-                        .font(.system(size: 24))
-                        .foregroundColor(.black)
-                        .padding(.trailing, 10)
+                .padding(.top, 40)
+                .padding(.bottom, 10)
+                .background(Color.white)
+                if showing == 0 {
+                    Landing()
+                }
+                else if showing == 1 {
+                    Resources()
                 }
             }
-            .padding(.top, 40)
-            .padding(.bottom, 10)
+            // for drag gesture...
+            .contentShape(Rectangle())
             .background(Color.white)
-            if showing == 0 {
-                Landing()
-            }
-            else if showing == 1 {
-                Resources()
-            }
         }
-        // for drag gesture...
-        .contentShape(Rectangle())
-        .background(Color.white)
     }
+}
+
+struct CaptureImageView {
+    
+    /// MARK: - Properties
+    @Binding var isShown: Bool
+    @Binding var image: Image?
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShown: $isShown, image: $image)
+    }
+}
+
+extension CaptureImageView: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController,
+                                context: UIViewControllerRepresentableContext<CaptureImageView>) {
+        
+    }
+    
 }
