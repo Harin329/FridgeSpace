a62a41553fea39628189d3988c4513a4df42f05c
Tiger Hao <tiger.hao@shopify.com>
2021-01-16 01:55:26 -0800

    donations api

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,31 @@
-import os
-from flask import Flask, jsonify
-from sqlalchemy.orm import sessionmaker
-from cockroachdb.sqlalchemy import run_transaction
-from models import db, User, Donation, FoodItem
+from flask import jsonify
+from models import app, db, User, Donation, FoodItem
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
-with app.app_context():
-    sessionmaker = sessionmaker(db.engine)
-    run_transaction(sessionmaker, lambda _: db.create_all())
+db.create_all()
 
 
 @app.route('/api/healthcheck')
 def healthcheck():
     return jsonify(status='success')
+
+
+@app.route('/api/donations', methods=['POST'])
+def create_donation():
+    food_item_ids = request.json.get('foodItems')
+
+    if not food_item_ids:
+        return jsonify(status='failure', message='Donation must include food items')
+
+    donation = Donation(
+        donor_id=test_user.id,
+        food_items=FoodItem.query.filter(FoodItem.id.in_(food_item_ids)).all()
+    )
+
+    return jsonify(
+        status='success',
+        donation=donation
+    )
 
 
 @app.route('/api/donations', methods=['GET'])
@@ -26,3 +35,28 @@ def get_donations():
         status='success',
         donations=list(map(Donation.to_json, donations))
     )
+
+
+def generate_mock_data():
+    test_user = User(name='test user')
+    db.session.add(test_user)
+    db.session.commit()
+
+    food_items = [FoodItem.query.filter_by(name=name).first() or FoodItem(name=name)
+                  for name in ('Cabbage', 'Carrot', 'Apple', 'Banana', 'Avocado', 'Mac & Cheese')]
+    donation_1 = Donation(
+        donor_id=test_user.id,
+        food_items=food_items[:4]
+    )
+    donation_2 = Donation(
+        donor_id=test_user.id,
+        food_items=food_items[3:]
+    )
+    db.session.add(donation_1)
+    db.session.add(donation_2)
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    generate_mock_data()
+    app.run()
