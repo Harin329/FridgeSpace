f7f6fd7fc282b902b917edf883025ff70cb37286
victor-uemura <uemura.v@gmail.com>
2021-01-16 14:35:51 -0800

    finished struct for detailed view

--- SwiftUI/Team/Map/DetailedCard.swift
+++ SwiftUI/Team/Map/DetailedCard.swift
@@ -10,17 +10,103 @@ import SwiftUI
 struct DetailedCard: View {
     var body: some View {
         VStack {
+            Capsule()
+                .foregroundColor(Color(hex: "C4C4C4"))
+                .frame(width: 40, height: 5)
             HStack {
                 Image("detailedView")
                 VStack {
-                    Text("Downtown Eastside Women’s Centre")
+                    HStack {
+                        Text("Downtown Eastside Women’s Centre")
+                            .font(.subheadline)
+                            .foregroundColor(Color(hex: "255359"))
+                        Spacer()
+                    }
                     HStack{
                         Text("4.0")
-                        
+                            .font(.subheadline)
+                            .foregroundColor(Color(hex: "3E6E79"))
+                        ForEach(1..<5+1) { number in
+                            if number < 5 {
+                                Image(systemName: "star.fill")
+                                    .font(.system(size: 13))
+                                    .foregroundColor(Color(hex: "FFC331"))
+                                    .padding(-3)
+                            }
+                            else {
+                                Image(systemName: "star")
+                                    .font(.system(size: 13))
+                                    .foregroundColor(Color(hex: "FFC331"))
+                                    .padding(-3)
+                            }
+                        }
+                        Text("(97)")
+                            .font(.subheadline)
+                            .foregroundColor(Color(hex: "3E6E79"))
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Women’s shelter • 302 Columbia St")
+                            .font(.subheadline)
+
+                            .foregroundColor(Color(hex: "3E6E79"))
+                        Spacer()
                     }
                 }
             }
+            HStack {
+                Text("See what’s in demand:")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "255359"))
+                Spacer()
+            }
+            HStack {
+                Text("Non-perishable")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "fff"))
+                    .padding(.vertical, 8)
+                    .padding(.horizontal,12)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color(hex: "9CC3AC")))
+                Text("Fresh Fruit")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "fff"))
+                    .padding(.vertical, 8)
+                    .padding(.horizontal,12)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color(hex: "FFC331")))
+                Text("Vegetable")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "fff"))
+                    .padding(.vertical, 8)
+                    .padding(.horizontal,11)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color(hex: "5590C3")))
+            }
+            HStack {
+                Text("24")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "9CC3AC"))
+                Text("households donated here today!")
+                    .font(.subheadline)
+                    .foregroundColor(Color(hex: "255359"))
+                Spacer()
+            }
+            Button(action: {
+            }) {
+                HStack {
+                    Text("Donate")
+                        .foregroundColor(Color(hex: "#fff"))
+                        .padding([.vertical],15)
+                        .frame(width:200)
+                }.background(Color(hex: "#255359"))
+            }.cornerRadius(30).padding(.top, 5)
         }
+        .cornerRadius(20, corners: [.topLeft, .topRight])
+        .padding(.horizontal, 20)
+        .padding(.top, 10)
+        .padding(.bottom, 15)
+        .background(Rectangle()
+        .fill(Color.white)
+        .cornerRadius(20, corners: [.topLeft, .topRight])
+        .shadow(radius: 8))
     }
 }
 
